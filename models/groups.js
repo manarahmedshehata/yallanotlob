75e1313da06e473df5b095ec0e927b04e6fcefcd
ehabzaki <m.ehabzaki@gmail.com>
2017-04-03 14:37:40 -0400

    groups and orders model

--- models/groups.js
+++ models/groups.js
@@ -1,1 +1,12 @@
 //should have string name and should have owner (object_id)
+var mongoose=require("mongoose")
+// register model
+var Schema=mongoose.Schema
+var groups=new Schema({
+  name:String,
+  owner:String,
+  users:[String]
+
+})
+// ORM
+mongoose.model("groups",groups)
--- models/orders.js
+++ models/orders.js
@@ -1,1 +1,18 @@
 //shoud have owner
+var mongoose=require("mongoose")
+// register model
+var Schema=mongoose.Schema
+var orders=new Schema({
+  order_owner:String,
+  status:String,
+  order_for:String,
+  menu_image:String,
+  order_date:date,
+  resturant_name:String,
+  list_invited:[String],
+  list_joined:[String]
+  details:[items{name:String,amount:Number,Price:Number,comment:String}]
+
+})
+// ORM
+mongoose.model("orders",orders)
