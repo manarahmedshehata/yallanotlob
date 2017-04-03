d736cbb34155b6cdd4a383ffbe28c177b8626c81
ehabzaki <m.ehabzaki@gmail.com>
2017-04-03 14:12:07 -0400

    add users schema

--- models/users.js
+++ models/users.js
@@ -1,1 +1,13 @@
 //should have string name ,email
+var mongoose=require("mongoose")
+// register model
+var Schema=mongoose.Schema
+var users=new Schema({
+  name:String,
+  email:String,
+  password:String,
+  friends:[String],
+  groups:[String]
+})
+// ORM
+mongoose.model("users",users)
